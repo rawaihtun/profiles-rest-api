c66389144212f230a0d278d707d03cad4a1271ec
Etherhunt <rawaihtun.ct@gmail.com>
2020-08-12 23:35:27 +0800

    Final update

--- profiles_project/settings.py
+++ profiles_project/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '8qr^893d+kh_md2=)5#evd@4@=@wf-4(=+ew5)8o!0jn9q8btp'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(int(os.environ.get('DEBUG', 1)))
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['ec2-18-133-161-78.eu-west-2.compute.amazonaws.com', '127.0.0.1']
 
 
 # Application definition
@@ -123,3 +123,5 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 AUTH_USER_MODEL = 'profiles_api.UserProfile'
+
+STATIC_ROOT = 'static/'
